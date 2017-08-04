38ed207b8942caf488f05c696b267e3c720d6167
Jennifer Le Pham <jennifer.pham@glidewelldental.com>
2017-08-03 23:09:39 -0700

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'app-server', //determines html tag, must be unique
+    templateUrl: './server.component.html' //must be relative path for webpack to use
+})
+export class ServerComponent {
+
+}
